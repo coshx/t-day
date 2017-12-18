6912c1675e6102b5dfc36340f8ba83f4e3bdee2e
Ben Taitelbaum <ben@coshx.com>
2017-12-18 11:21:00 -0500

    Initial Commit

---
+++ T'Day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T'Day
+//
+//  Created by Ben Taitelbaum on 12/18/17.
+//  Copyright © 2017 Coshx Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
